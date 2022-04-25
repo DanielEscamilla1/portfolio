192c3b521e88a41a44d139e21d5d1c80db92ed7b
Daniel Escamilla <descamilla128@gmail.com>
2022-04-25 00:17:49 -0400

    working on dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-import './App.css';
-import Intro from './components/intro/Intro';
-import Aboutme from './components/aboutme/Aboutme'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
-import Navbar from './components/nav/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom'
-import ProjectList from './components/projectcards/ProjectList';
-
+import "./App.css";
+import Intro from "./components/intro/Intro";
+import Aboutme from "./components/aboutme/Aboutme";
+// import Projects from './components/projects/Projects'
+import Toggle from "./components/toggle/Toggle";
+import Contact from "./components/contact/Contact";
+import Navbar from "./components/nav/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import ProjectList from "./components/projectcards/ProjectList";
+import { useContext } from "react";
+import { ThemeContext } from "./context";
 
 function App() {
+
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
   return (
     <Router>
-      <Navbar />
-      <Intro />
-      <Aboutme />
-      <ProjectList />
-      <Contact />
+      <div style={{ backgroundColor:darkMode ? "#222" : "white", color:darkMode && "white" }}>
+        <Navbar />
+        <Toggle />
+        <Intro />
+        <Aboutme />
+        <ProjectList />
+        <Contact />
+      </div>
     </Router>
   );
 }
--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import "./Contact.css";
+import Phone from "../../img/Phone.png";
+import emailjs from "@emailjs/browser";
+import { useContext } from "react";
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
 
-export default Contact
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_yy0e1l8",
+        "template_gsvi04n",
+        formRef.current,
+        "IPGPIN5fXgd5w-uD2"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +1 123 456 7890
+            </div>
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              contact@lama.com
+            </div>
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              123 Unity st, Thomasville, North Carolina
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What's your story?</b>Get in touch. Always freelancing if the
+            right project comes along.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="First Name" name="user_name" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Last Name" name="user_subject" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
+            <textarea style={{backgroundColor: darkMode && "#333"}} name="message" rows="5" placeholder="Message"></textarea>
+            <button>Submit</button>
+            {done && " Thank you..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
