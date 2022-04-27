2a0dcd4653d31ea4beb7697107095625d72f13ec
Daniel Escamilla <descamilla128@gmail.com>
2022-04-26 21:33:48 -0400

    added resume

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Navbar.css";
+import Resume from '../../img/resume.png'
 
 const Navbar = () => {
   return (
     <>
-    
       <nav className="main-nav">
         <a className="de" href="#intro" id="de-text">DE</a>
         <a className="de" href="#about">About</a>
         <a className="de"href="#projects">Projects</a>
         <a className="de"href="#contact">Contact</a>
-        <button className="nav-btn">Resume</button>
+        <a href={Resume} download className='nav-btn'>Resume</a>
       </nav>
     </>
   );
